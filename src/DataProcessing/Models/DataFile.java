1f8855b0a3477f1654279f509edba0533708463f
Marco <meseddon-ferretti1@sheffield.ac.uk>
2021-07-29 07:29:36 +0100

    Polynomial curve fitting

--- src/DataProcessing/Models/DataFile.java
+++ src/DataProcessing/Models/DataFile.java
@@ -18,6 +18,8 @@ import java.util.stream.Collectors;
 
 public class DataFile {
 
+    //TODO figure out how to deal with corrected theta/energy
+
     private MeasurementType dataType; // The data type of the file (I0, It, I0b, Itb)
     private String filePath; // Path to the file in the user's file system
     private String header; // Header line(s) of file
@@ -57,6 +59,38 @@ public class DataFile {
                 "\t" + x.getCnts_per_live() + "\t").collect(Collectors.toList()));
         return stringData;
     }
+
+    /**
+     *
+     * @return A collection of all the measurements of energy in the data file
+     */
+    public List<Double> getEnergy() {
+        ArrayList<XRaySample> data = this.getData();
+        List<Double> energy = data.stream().map(x -> x.getEnergy()).collect(Collectors.toList());
+        return energy;
+    }
+
+    /**
+     *
+     * @return A collection of all the measurements of theta in the data file
+     */
+    public List<Double> getTheta() {
+        ArrayList<XRaySample> data = this.getData();
+        List<Double> theta = data.stream().map(x -> x.getTheta()).collect(Collectors.toList());
+        return theta;
+    }
+
+    /**
+     *
+     * @return A collection of all the measurements of theta in the data file
+     */
+    public List<Double> getCounts() {
+        ArrayList<XRaySample> data = this.getData();
+        List<Double> counts = data.stream().map(x -> x.getCnts_per_live()).collect(Collectors.toList());
+        return counts;
+    }
+
+
 
     public String getHeader() {
         return header;
--- src/DataProcessing/Processors/DataProcessor.java
+++ src/DataProcessing/Processors/DataProcessor.java
@@ -6,8 +6,10 @@ import DataProcessing.Models.XRaySample;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import org.apache.commons.math3.fitting.*;
 
 public class DataProcessor {
@@ -41,7 +43,8 @@ public class DataProcessor {
             meanSamples.add(meanSample);
         }
         File saveDirectory = new File(firstFile.getFilePath()).getParentFile();
-        String filePath = saveDirectory.getAbsolutePath() + "/" + fileName + ".txt";
+        //TODO dat file
+        String filePath = saveDirectory.getAbsolutePath() + System.getProperty("file.separator") + fileName + ".txt";
         return new DataFile(dataType, filePath, fileHeader, meanSamples);
     }
 
@@ -65,44 +68,77 @@ public class DataProcessor {
         return new XRaySample(meanEnergy, meanTheta, meanCounts);
     }
 
-    /**
-     *
-     * @param file
-     * @return String representation of polynomial fit to data file
-     */
-    public String generatePolynomial(DataFile file, int polyDegree) {
-        //TODO create test
-
-
-        PolynomialCurveFitter pcf = PolynomialCurveFitter.create(3);
-
-        // Store data as WeightedObservedPoints
+    //TODO theta or energy against counts? ENERGY
+    public double[] generatePoly(DataFile file, int polyDegree) {
         final WeightedObservedPoints obs = new WeightedObservedPoints();
-        for (XRaySample sample: file.getData()) {
-            obs.add(sample.getTheta(), sample.getCnts_per_live());
+        ArrayList<XRaySample> samples = file.getData();
+        for (int i = 0; i < samples.size(); i++) {
+            XRaySample sample = samples.get(i);
+            obs.add(sample.getEnergy(), sample.getCnts_per_live());
         }
-        final PolynomialCurveFitter fitter
-                = PolynomialCurveFitter.create(0).withStartPoint(new double[] { -1e-20, 3e15, -5e25 });
+        // Instantiate a third-degree polynomial fitter.
+        final PolynomialCurveFitter fitter = PolynomialCurveFitter.create(3);
 
-        final double[] best = fitter.fit(obs.toList());
-
-
-
-        return "";
+        // Retrieve fitted parameters (coefficients of the polynomial function).
+        final double[] coeff = fitter.fit(obs.toList());
+        return coeff;
     }
+
+    //TODO theta or energy against counts? ENERGY
+    public double[] generatePolyTest() {
+        final WeightedObservedPoints obs = new WeightedObservedPoints();
+        for (int i = 1; i < 1000; i++) {
+            obs.add(i, i*i);
+        }
+        //ArrayList<XRaySample> samples = file.getData();
+        /*
+        for (int i = 0; i < samples.size(); i++) {
+            XRaySample sample = samples.get(i);
+            obs.add(sample.getEnergy(), sample.getCnts_per_live());
+        }
+
+         */
+        // Instantiate a third-degree polynomial fitter.
+        final PolynomialCurveFitter fitter = PolynomialCurveFitter.create(3);
+
+        // Retrieve fitted parameters (coefficients of the polynomial function).
+        final double[] coeff = fitter.fit(obs.toList());
+        return coeff;
+    }
+
 
     /**
      *
-     * @return Value of absorption (for file?)
+     * @return ArrayList of absorption values
      */
-    public int calculateAbsorption(DataFile i0, DataFile it) {
+    public ArrayList<Double> calculateAbsorption(DataFile i0File, DataFile itFile) {
+        //TODO check data ranges match
+        List<Double> it_counts = itFile.getCounts();
+        Iterator<Double> i0 = i0File.getCounts().iterator();
+        Iterator<Double> it = itFile.getCounts().iterator();
+
+        ArrayList<Double> absorption = new ArrayList<>();
+        while (i0.hasNext() && it.hasNext()) {
+            absorption.add(Math.log(i0.next() / it.next()));
+        }
         //TODO method body
-        return 0;
+        return absorption;
     }
 
-    public int calculateAbsorption(DataFile i0, DataFile it, DataFile i0b, DataFile itb) {
+    public List<Double> calculateAbsorption(DataFile i0File, DataFile itFile, DataFile i0bFile, DataFile itbFile) {
         //TODO method body
-        return 0;
+        List<Double> it_counts = itFile.getCounts();
+        Iterator<Double> i0 = i0File.getCounts().iterator();
+        Iterator<Double> it = itFile.getCounts().iterator();
+        Iterator<Double> i0b = i0bFile.getCounts().iterator();
+        Iterator<Double> itb = itbFile.getCounts().iterator();
+
+        ArrayList<Double> absorption = new ArrayList<>();
+        while (i0.hasNext() && it.hasNext()) {
+            double a = Math.log((i0.next() - i0b.next()) / (it.next() - itb.next()));
+            absorption.add(a);
+        }
+        return absorption;
     }
 
     /**
@@ -110,5 +146,40 @@ public class DataProcessor {
      */
     public void correctThetaScale(DataFile file) {
         //
+    }
+
+    //TODO check if this way of validating file ranges is acceptable (possibly able to check if two <Double> ArrayLists are identical?)
+    //TODO test
+
+    /**
+     *
+     * @param files
+     * @return true if all files are same length, and have same values for first and last energy
+     */
+    public boolean checkRanges(DataFile... files) {
+        if (files.length == 0 || files.length == 1)
+            return true;
+        List<Double> file0_energy = files[0].getEnergy();
+        int size = file0_energy.size();
+        int lastIndex = file0_energy.size() -1;
+        double energy1 = file0_energy.get(0);
+        double energy2 = file0_energy.get(file0_energy.size()-1);
+
+        for (DataFile file: files) {
+            if (file.getEnergy().size() != size) // Check that data sets are same size
+                return false;
+
+            double e1 = file.getEnergy().get(0);
+            double e2 = file.getEnergy().get(lastIndex);
+            if (e1 != energy1 || e2 != energy2) //Check that first and last energy values match
+                return false;
+
+            /*
+                More concise implementation?
+            if (!file.getEnergy().equals(file0_energy])
+                return false;
+             */
+        }
+        return true;
     }
 }
