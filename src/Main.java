ccfa5019fe1705a8339af6378a2d6bdf8e54999e
Shiny42 <shiftbee@gmail.com>
2021-09-09 13:14:56 +0100

    Changed look and feel to match system style

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
 import GUI.*;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
         GUI gui = new GUI("Hermes");
+
     }
 
 }
